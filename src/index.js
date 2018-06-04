7d9d7358147a7a916bbf8e11356db4ec14b3d89a
FriseHunter <freesoul2708@gmail.com>
2018-06-04 23:52:20 +0300

    'ClockLiveWhile'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './App.css'
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+class Clock extends React.Component {
+  constructor (props) {
+  super (props);
+  this.state = {
+    date : new Date()
+  };
+}
+
+//Запуск setInterval() после создания компонента
+
+componentDidMount() {
+  this.timerId = setInterval(() => this.tick(), 1000)
+}
+
+// очистка интервала перед удалением компонента 
+
+componentWillUnmount() {
+  clearInterval(this.timerId);
+}
+
+//алгоритм исполняющий обновление setInterval() раз в секунду
+
+tick() {
+  this.setState({
+    date: new Date()
+  });
+}
+  render(){
+    return (
+      <div className="container">
+        <h1 className="caption">Hello, world!</h1>
+        <h2>It is 
+          <div className="beforeTime">
+            <span className="time">{this.state.date.toLocaleTimeString()}</span>
+          </div>
+        </h2>
+      </div>
+    );
+  }
+}
+
+  ReactDOM.render(
+    <Clock/>,
+    document.getElementById('root')
+  );
